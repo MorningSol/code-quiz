24d974e77187bc160c3dc68c744578d3c80174b3
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-03-13 19:18:00 -0400

    display correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
 var startButtonEl = document.querySelector("#start-btn");
 var timerEl = document.querySelector("#timer-time");
 var quizStartSection = document.querySelector("#quiz-start");
-var questionOne = document.querySelector("#question1");
-var questionTwo = document.querySelector("#question2");
-var questionThree = document.querySelector("#question3");
-var questionFour = document.querySelector("#question4");
-var allQuestions = document.querySelectorAll(".quiz-box")
+var question1El = document.querySelector("#question1");
+var question2El = document.querySelector("#question2");
+var question3El = document.querySelector("#question3");
+var question4El = document.querySelector("#question4");
+var allQuestions = document.querySelectorAll(".quiz-box");
+var buttonSelectorEl = document.querySelector("#main");
+var answerFeedback = document.querySelectorAll(".result")
 var timeLeft = 0;
+
+
+
+var taskButtonHandler = function(event){
+    var choiceEl = event.target;
+    var x = 0
+
+    if (choiceEl.matches(".correct")){
+        console.log("correct")
+        answerFeedback[x].textContent = "CORRECT!";
+                  
+    };
+    if (choiceEl.matches(".wrong")){
+        console.log("wrong")
+        answerFeedback[x].textContent = "INCORRECT!";
+    };
+    x++
+};
 
 
 var countdown = function(){
@@ -20,14 +40,14 @@ var countdown = function(){
             clearInterval(timerInterval);
             gameOver()
         } 
-    }, 100);
+    }, 1000);
 };
 
 
 var startQuiz = function() {
    
     quizStartSection.style.display = "none";
-    questionOne.style.display = "flex";
+    question1El.style.display = "flex";
     countdown()
 };
 
@@ -37,12 +57,13 @@ var gameOver = function() {
         allQuestions[i].style.display = "none"
     };
 
-    
-
+    alert("You have run out of time and your score is 0.  Please try again")
+    quizStartSection.style.display = "flex";
 };
 
 
 
 
 
-startButtonEl.addEventListener("click", startQuiz)
+startButtonEl.addEventListener("click", startQuiz);
+buttonSelectorEl.addEventListener("click", taskButtonHandler);
