857665430be78a2c368c6107a43509b200ab2b08
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-03-14 15:36:45 -0400

    added results page and score posting

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,38 +5,43 @@ var question1El = document.querySelector("#question1");
 var question2El = document.querySelector("#question2");
 var question3El = document.querySelector("#question3");
 var question4El = document.querySelector("#question4");
+var quizResultsEl = document.querySelector("#quiz-results");
 var allQuestions = document.querySelectorAll(".quiz-box");
 var buttonQuestion1El = document.querySelector("#btn-question1");
 var buttonQuestion2El = document.querySelector("#btn-question2");
 var buttonQuestion3El = document.querySelector("#btn-question3");
 var buttonQuestion4El = document.querySelector("#btn-question4");
-var answerFeedback = document.querySelectorAll(".result")
+var postScoreEl = document.querySelector("#score")
+var answerFeedback = document.querySelectorAll(".result");
 var timeLeft = 0;
-var x = 0;
+var questionProgression = 0;
 var timerInterval;
+var currentScore = 0;
 
 var nextQuestion = function() {
-    switch (x) {
+    switch (questionProgression) {
     case 0:
         question1El.style.display = "none";
         question2El.style.display = "flex";
-        x++
+        questionProgression++
         break;
     case 1: 
         question2El.style.display = "none";
         question3El.style.display = "flex";
-        x++
+        questionProgression++
         break;
         
     case 2:
         question3El.style.display = "none";
         question4El.style.display = "flex";
-        x++
+        questionProgression++
         break;
     case 3:
         question4El.style.display = "none";
+        quizResultsEl.style.display = "flex";
         clearInterval(timerInterval)
-        
+        postScoreEl.textContent = timerEl.textContent;
+        currentScore = timerEl.textContent   
     };             
 };
 
@@ -45,17 +50,17 @@ var taskButtonHandler = function(event){
     
     if (choiceEl.matches(".correct")){
         console.log("correct")
-        answerFeedback[x].textContent = "CORRECT!";
+        answerFeedback[questionProgression].textContent = "CORRECT!";
           
     };
     if (choiceEl.matches(".wrong")){
         console.log("wrong")
-        answerFeedback[x].textContent = "INCORRECT!";
+        answerFeedback[questionProgression].textContent = "INCORRECT!";
        
         timeLeft -= 10;
     };
     
-    setTimeout(function() {nextQuestion()},1000);
+    setTimeout(function() {nextQuestion()},900);
 };
 
 
