0f3b96e1451c030827925e5e8c251ce66169a22a
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-03-15 17:25:33 -0400

    post scores, save scores and load scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,23 @@ var question1El = document.querySelector("#question1");
 var question2El = document.querySelector("#question2");
 var question3El = document.querySelector("#question3");
 var question4El = document.querySelector("#question4");
-var quizResultsEl = document.querySelector("#quiz-results");
+var quizResultsEl = document.querySelector("#results-page");
+var highscorePageEl = document.querySelector("#highscore-page");
 var allQuestions = document.querySelectorAll(".quiz-box");
+var resultsFormEl = document.querySelector("#quiz-results")
 var buttonQuestion1El = document.querySelector("#btn-question1");
 var buttonQuestion2El = document.querySelector("#btn-question2");
 var buttonQuestion3El = document.querySelector("#btn-question3");
 var buttonQuestion4El = document.querySelector("#btn-question4");
-var postScoreEl = document.querySelector("#score")
+var postScoreEl = document.querySelector("#score");
 var answerFeedback = document.querySelectorAll(".result");
 var timeLeft = 0;
 var questionProgression = 0;
 var timerInterval;
 var currentScore = 0;
+var scoreIdCounter = 0;
+var scoreSheet = document.querySelector("#score-sheet");
+var highscoreArr = [];
 
 var nextQuestion = function() {
     switch (questionProgression) {
@@ -98,8 +103,64 @@ var gameOver = function() {
     quizStartSection.style.display = "flex";
 };
 
+var scoreInfoHandler = function(event){
+    event.preventDefault();
+    var initialInput = document.querySelector("input[name = 'player-initials']").value;
 
+    if (!initialInput){
+        alert("You need to input your initials!");
+        return false
+    }
 
+    resultsFormEl.reset();
+
+    initialInput = initialInput.toUpperCase();
+
+    var playerInfoObj = {
+        initials: initialInput,
+        score: currentScore
+    };
+    quizResultsEl.style.display = "none";
+    highscorePageEl.style.display = "flex";
+
+    createScoreInfoEl(playerInfoObj);
+};
+
+createScoreInfoEl = function(playerInfoObj){
+    var scoreListItem = document.createElement("li");
+    scoreListItem.setAttribute("data-score-id", scoreIdCounter)
+
+    scoreListItem.innerHTML = playerInfoObj.initials + " - " + playerInfoObj.score
+    scoreSheet.appendChild(scoreListItem);
+
+    playerInfoObj.id = scoreIdCounter;
+    
+    
+    highscoreArr.push(playerInfoObj);
+    
+    saveScores();
+
+    scoreIdCounter++
+}
+
+var saveScores = function(){
+    localStorage.setItem("highscoreArr", JSON.stringify(highscoreArr));
+};
+
+var loadScores = function(){
+    var scoresLoaded = localStorage.getItem("highscoreArr");
+
+    if (!scoresLoaded) {
+        return false;
+    };
+
+    scoresLoaded = JSON.parse(scoresLoaded);
+
+    for (var i = 0; i < scoresLoaded.length; i++) {
+        createScoreInfoEl(scoresLoaded[i]);
+    };
+
+};
 
 
 startButtonEl.addEventListener("click", startQuiz);
@@ -107,3 +168,6 @@ buttonQuestion1El.addEventListener("click", taskButtonHandler);
 buttonQuestion2El.addEventListener("click", taskButtonHandler);
 buttonQuestion3El.addEventListener("click", taskButtonHandler);
 buttonQuestion4El.addEventListener("click", taskButtonHandler);
+quizResultsEl.addEventListener("submit",scoreInfoHandler);
+
+loadScores();
